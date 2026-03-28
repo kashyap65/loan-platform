c57a643feaf01a7e902ec85799556dbf0481c5b8
kashyap65 <kashyappandey495@gmail.com>
2026-03-28 21:51:26 +0530

    git commit -m "feat: add loan-application-service module"

---
+++ loan-application-service/src/main/java/com/loanplatform/loanapplication/LoanApplicationServiceApplication.java
@@ -0,0 +1,11 @@
+package com.loanplatform.loanapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplicationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApplicationServiceApplication.class,args);
+    }
+}
